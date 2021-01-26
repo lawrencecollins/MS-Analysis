e05a703c3b3ec84eb483ca93353a6213c6ca2af3
Lawrence Collins <67695762+lawrencecollins@users.noreply.github.com>
2021-01-26 09:29:26 +0000

    .

--- initiate_unidec.py
+++ initiate_unidec.py
@@ -1,1 +1,23 @@
 from UniDec import unidec
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+
+
+mass_range = [11500.0, 13000.0]
+# load text file
+eng = unidec.UniDec()
+eng.open_file("mean_data.txt")
+
+eng.process_data()
+eng.run_unidec()
+eng.pick_peaks()
+eng.process_mass_data()
+
+mass_dist_data = np.loadtxt("mean_data_unidecfiles/mean_data_mass.txt")
+
+print(mass_dist_data.shape)
